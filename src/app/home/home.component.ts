b6678d729082de82b3cd93d65551f0239eb47891
Igor Pierre <62574338+ipcm27@users.noreply.github.com>
2022-08-30 22:02:27 -0300

    No longer have callback hell, now the data its no longer avaible on state variables (instead we only have observables)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,51 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-
+import { CourseService } from "./../services/courses.service";
+import { Component, OnInit } from "@angular/core";
+import { Course, sortCoursesBySeqNo } from "../model/course";
+import { interval, noop, Observable, of, throwError, timer } from "rxjs";
+import {
+  catchError,
+  delay,
+  delayWhen,
+  filter,
+  finalize,
+  map,
+  retryWhen,
+  shareReplay,
+  tap
+} from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
 
-  beginnerCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
-
-
-  constructor(private http: HttpClient, private dialog: MatDialog) {
-
-  }
+  constructor(
+    private coursesService: CourseService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
+    const Courses$ = this.coursesService
+      .loadAllCourses()
+      .pipe(map(courses => courses.sort(sortCoursesBySeqNo)));
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
-
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+    this.beginnerCourses$ = Courses$.pipe(
+      map(courses => courses.filter(course => course.category == "BEGINNER"))
+    );
+    this.advancedCourses$ = Courses$.pipe(
+      map(courses => courses.filter(course => course.category == "ADVANCED"))
+    );
   }
 
   editCourse(course: Course) {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
@@ -50,11 +55,5 @@ export class HomeComponent implements OnInit {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
-
 }
-
-
-
-
